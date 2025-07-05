da5b7fc1ccfae6f095db3c2593f7adf77f620e05
Ville Kautiainen <kautiainen.ville@gmail.com>
2025-07-05 10:21:57 +0300

    Updated colors

--- lua/skaupat/colors.lua
+++ lua/skaupat/colors.lua
@@ -3,15 +3,31 @@ local M = {}
 function M.setup(style)
   local c = {}
 
-  if style == "light" then
-    c.bg      = "#F5F5F5"
-    c.fg      = "#1E1E1E"
-    c.green   = "#007A33"
-    c.lgreen  = "#B4D8C1"
-    c.dgreen  = "#005026"
-    c.gray    = "#CCCCCC"
-    c.comment = "#999999"
-  end
+  -- Base background and foreground
+  c.bg         = "#ffffff"  -- or #f5f5f5
+  c.bg_alt     = "#f6f6f6"
+  c.bg_highlight = "#ebf7ec"
+
+  c.fg         = "#1f1f1f"
+  c.fg_alt     = "#484848"
+  c.comment    = "#949494"
+
+  -- Accents
+  c.blue       = "#3974bc"
+  c.green      = "#007841"
+  c.teal       = "#006e87"
+  c.brown      = "#885600"
+  c.red        = "#b70000"
+  c.yellow     = "#fff781"
+  c.error_bg   = "#ffeded"
+
+  -- UI and special
+  c.selection  = "#e4f5e8"
+  c.cursorline = "#f1f1f1"
+  c.statusline = "#007841"
+  c.visual     = "#d2fcd5"
+  c.diff_add   = "#ebf7ec"
+  c.diff_delete = "#ffeded"
 
   return c
 end
